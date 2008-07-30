d1d5dc943eb849a91e96c307e2f981db52132efd
Andrew Neil <andrew.jr.neil@gmail.com>
2008-07-31 00:15:09 +0100

    Wrote README with intro, installation and usage sections. Appended .rdoc extension to README (for pretty formatting on github ).

--- file_system_extension.rb
+++ file_system_extension.rb
@@ -4,7 +4,7 @@
 class FileSystemExtension < Radiant::Extension
   version "0.1"
   description "Load Radiant models from the filesystem"
-  url "http://code.digitalpulp.com"
+  url "http://github.com/nelstrom/radiant-file-system-extension/tree/master"
   
   def activate
     FileSystem.activate
